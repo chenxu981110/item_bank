bbe013ccedf634808aa93d87c99f437b2093326c
newbie-ma99 <1095536858@qq.com>
2022-01-05 09:27:39 +0800

    Signed-off-by: newbie-ma99 <1095536858@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'Login',
     component: Home
   },
   {
