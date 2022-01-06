145873c44b317302d0c8d021a18cee84e1828315
newbie-ma99 <1095536858@qq.com>
2022-01-06 15:58:24 +0800

    试卷

---
+++ src/views/Exam/Exam.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Exam">
+    <div>一、判断题(每小题10.0分，共2题, 共 20.0 分)</div>
+    <div>二 、单选题(每小题12.0分, 共2题, 共 24.0 分)</div>
+    <div>三 、多选题(每小题20.0分，共1题, 共 20.0 分)</div>
+    <div>四 、作答题(共2题, 共 40.0 分)</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
